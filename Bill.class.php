2bb1ee966066cfe52e74db2e146c6c012a08c821
Anton Popov <sleuthhound@yandex.ru>
2015-04-11 20:06:45 +0500

    Начальная версия класса для работы с счетами.

--- Bill.class.php
+++ Bill.class.php
@@ -11,36 +11,58 @@ namespace RegRu;
 
 /**
  * Класс для работы cо счетами
+ * 
+ * @link https://www.reg.ru/support/help/api2#bill_functions
  */
 class Bill extends Api
 {
     /**
      * Тестовый метод, доступен клиентам
+     * 
+     * @link https://www.reg.ru/support/help/api2#bill_nop
      */
-    public function nop()
-    {}
+    public function nop($billId = null, array $bills = array())
+    {
+        return $this->send(get_defined_vars());
+    }
     
     /**
      * Метод для просмотра всех неоплаченных счетов, доступен клиентам
+     * 
+     * @link https://www.reg.ru/support/help/api2#bill_get_not_payed
      */
-    public function getNotPayed()
-    {}
+    public function getNotPayed($limit = null, $offset = null)
+    {
+        return $this->send(get_defined_vars());
+    }
     
     /**
      * Метод для просмотра счетов за указанный период, доступен партнерам
+     * 
+     * @link https://www.reg.ru/support/help/api2#bill_get_for_period
      */
-    public function getForPeriod()
-    {}
+    public function getForPeriod($startDate, $endDate, $payType = null, $limit = null, $offset = null, $all = null)
+    {
+        return $this->send(get_defined_vars());
+    }
     
     /**
      * Метод для смены способа оплаты, доступен клиентам
+     * 
+     * @link https://www.reg.ru/support/help/api2#bill_change_pay_type
      */
-    public function changePayType()
-    {}
+    public function changePayType($billId = null, array $bills = array(), $payType, $currency = 'RUR', $wmid = null)
+    {
+        return $this->send(get_defined_vars());
+    }
     
     /**
      * Метод для удаления неоплаченных счетов, доступен клиентам
+     * 
+     * @link https://www.reg.ru/support/help/api2#bill_delete
      */
-    public function delete()
-    {}
+    public function delete($billId = null, array $bills = array())
+    {
+        return $this->send(get_defined_vars());
+    }
 }
